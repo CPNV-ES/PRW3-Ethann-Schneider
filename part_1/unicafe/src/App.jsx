3a90d1e9c567604a802cc7f33ccbfd46f878c7ba
EthannSchneider <ethann@shkermit.ch>
2026-02-04 11:25:31 +0100

    Added excercice 1.6 to 1.11

---
+++ part_1/unicafe/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Statistics from './components/Statistics'
+import Button from './components/Button'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h2>give feedback</h2>
+      <main>
+        <div>
+          <Button setValue={setGood} value={good} text="good" />
+          <Button setValue={setNeutral} value={neutral} text="neutral" />
+          <Button setValue={setBad} value={bad} text="bad" />
+        </div>
+
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </main>
+    </>
+  )
+}
+
+export default App
